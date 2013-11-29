65ac5547d35e62c2a15fba4eeb978a158baae354
Diogo Biazus <diogob@gmail.com>
2013-11-29 17:17:55 -0200

    removed format (we'll use only json) and added API secret configuration

--- lib/moip_v2/configuration.rb
+++ lib/moip_v2/configuration.rb
@@ -1,15 +1,15 @@
 module MoipV2
   module Configuration
     VALID_CONNECTION_KEYS = [:endpoint, :user_agent, :method].freeze
-    VALID_OPTIONS_KEYS    = [:api_key, :format].freeze
+    VALID_OPTIONS_KEYS    = [:api_secret, :api_key].freeze
     VALID_CONFIG_KEYS     = VALID_CONNECTION_KEYS + VALID_OPTIONS_KEYS
  
     DEFAULT_ENDPOINT    = 'http://awesome.dev/api'
     DEFAULT_METHOD      = :get
     DEFAULT_USER_AGENT  = "Awesome API Ruby Gem #{MoipV2::VERSION}".freeze
  
-    DEFAULT_API_KEY      = nil
-    DEFAULT_FORMAT       = :json
+    DEFAULT_API_KEY     = nil
+    DEFAULT_API_SECRET  = nil
  
     # Build accessor methods for every config options so we can do this, for example:
     #   Awesome.format = <img src="http://s2.wp.com/wp-includes/images/smilies/icon_mad.gif?m=1129645325g" alt=":x" class="wp-smiley"> ml
@@ -34,7 +34,7 @@ module MoipV2
       self.user_agent = DEFAULT_USER_AGENT
 
       self.api_key    = DEFAULT_API_KEY
-      self.format     = DEFAULT_FORMAT
+      self.api_secret = DEFAULT_API_SECRET
     end
 
   end # Configuration
